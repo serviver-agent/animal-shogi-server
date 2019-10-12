bb345ee804a53e1e5995eb8a1ec03870ce1750db
KazuyaMiayshita <miy1924@gmail.com>
2019-10-13 03:47:35 +0900

    将棋盤の上の駒はその位置から一意に特定できるのでIDなんていりませーん

--- domain/rules/src/main/scala/shogiban/PlacedKoma.scala
+++ domain/rules/src/main/scala/shogiban/PlacedKoma.scala
@@ -2,9 +2,7 @@ package domain.rules.shogiban
 
 import domain.rules.koma.Koma
 
-case class PlacedKomaId(value: Int)
 case class PlacedKoma(
-  id: PlacedKomaId,
   player: Player,
   position: Position,
   koma: Koma
--- domain/rules/src/main/scala/shogiban/Shogiban.scala
+++ domain/rules/src/main/scala/shogiban/Shogiban.scala
@@ -12,14 +12,14 @@ object Shogiban {
     import domain.rules.koma._
 
     val placedKomas = Seq(
-      PlacedKoma(PlacedKomaId(1), Gote,  Position(PosA, Pos1), Kirin),
-      PlacedKoma(PlacedKomaId(2), Gote,  Position(PosB, Pos1), Raion),
-      PlacedKoma(PlacedKomaId(3), Gote,  Position(PosC, Pos1), Zou),
-      PlacedKoma(PlacedKomaId(4), Gote,  Position(PosB, Pos2), Hiyoko),
-      PlacedKoma(PlacedKomaId(5), Sente, Position(PosB, Pos3), Hiyoko),
-      PlacedKoma(PlacedKomaId(6), Sente, Position(PosA, Pos4), Zou),
-      PlacedKoma(PlacedKomaId(7), Sente, Position(PosB, Pos4), Raion),
-      PlacedKoma(PlacedKomaId(8), Sente, Position(PosC, Pos4), Kirin),
+      PlacedKoma(Gote,  Position(PosA, Pos1), Kirin),
+      PlacedKoma(Gote,  Position(PosB, Pos1), Raion),
+      PlacedKoma(Gote,  Position(PosC, Pos1), Zou),
+      PlacedKoma(Gote,  Position(PosB, Pos2), Hiyoko),
+      PlacedKoma(Sente, Position(PosB, Pos3), Hiyoko),
+      PlacedKoma(Sente, Position(PosA, Pos4), Zou),
+      PlacedKoma(Sente, Position(PosB, Pos4), Raion),
+      PlacedKoma(Sente, Position(PosC, Pos4), Kirin),
     )
     Shogiban(placedKomas)
   }
